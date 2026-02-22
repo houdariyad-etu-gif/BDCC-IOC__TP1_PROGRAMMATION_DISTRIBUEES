37416527bc2ea85fcfb4537c5b2a5f6e9ee6dbef
Houda <houda.riyad-etu@etu.univh2c.ma>
2026-02-22 17:36:57 +0000

    specifying annotations

--- TP1/src/main/java/net/riyad/ext/DaoImplV2.java
+++ TP1/src/main/java/net/riyad/ext/DaoImplV2.java
@@ -3,7 +3,8 @@ package net.riyad.ext;
 import net.riyad.dao.IDao;
 import org.springframework.stereotype.Component;
 
-@Component("d2")
+//@Component("d2") <--- @Component : is general .for the dao class we do @Repository
+@Repository("d2")
 public class DaoImplV2 implements IDao {
 
     @Override
